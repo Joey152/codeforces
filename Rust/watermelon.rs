0cdf68e50af4ae681a8fa4d99ae077c13223743e
Joey Chan <joseph.chan@ryerson.ca>
2017-11-21 18:36:27 -0500

    1

---
+++ Rust/watermelon.rs
@@ -0,0 +1,13 @@
+use std::io;
+
+fn main() {
+    let mut line = String::new();
+    io::stdin().read_line(&mut line).unwrap();
+    let line: i32 = line.trim().parse().unwrap();
+    if line <= 2 || line % 2 != 0 {
+        println!("NO");
+    }
+    else {
+        println!("YES");
+    }
+}
